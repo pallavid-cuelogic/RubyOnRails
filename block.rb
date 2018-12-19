87e92e64bb8fca3595d1b246b1fbd2fd319164bd
pallavid-cuelogic <pallavi.deshmukh@cuelogic.com>
2018-12-19 13:30:24 +0530

    new files addded with block examples

---
+++ block.rb
@@ -0,0 +1,8 @@
+def my_method
+    puts "In Method"
+    yield
+    yield
+    puts "In Method again"
+end
+
+my_method{ puts 'In Block' }
---
+++ blockWith_Params.rb
@@ -0,0 +1,6 @@
+def one_two_three
+    yield 5
+    yield 5
+    yield 5
+  end
+  one_two_three { |number| puts number + 2 }
