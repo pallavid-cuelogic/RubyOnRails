4cdddd7a3335fcd6de9896fe09b8b96fc3266294
pallavid-cuelogic <pallavi.deshmukh@cuelogic.com>
2019-01-10 11:41:17 +0530

    commiting latest changes to git 10 Jan 12 AM

--- EmployeeInfo/app/models/employee.rb
+++ EmployeeInfo/app/models/employee.rb
@@ -8,6 +8,9 @@ class Employee < ActiveRecord::Base
 	def update_employee
 		put "abc"
 	end
-
+	
+	# when we want to write code that is very much specific to database 
+	# then it should not be written in Controller's class it will go in 
+	# model's class
 
 end
